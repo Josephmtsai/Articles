9d6105994fcfba06c0cb6cadb47c787ed81f28c0
Unknown <mmx362003@gmail.com>
2019-03-08 01:03:19 +0800

    ad

--- rsshandler/rsshandler.py
+++ rsshandler/rsshandler.py
@@ -1,12 +1,18 @@
 import feedparser
+from dbHandler import rssFeedHandler
 
 
 def parser(url):
-    rssDataList = feedparser.parse(url)
-    print(rssDataList['feed'])
-    for rssData in rssDataList['entries']:
-        print(rssData)
-        break
+    rssFeedList = feedparser.parse(url)
+    mongoRssDataList = []
+
+    for rssFeed in rssFeedList['entries']:
+        mongoRssData = {}
+        mongoRssData['title'] = rssFeed['title']
+        mongoRssData['description'] = rssFeed['description']
+        mongoRssData['links'] = rssFeed['links'][0].href
+        mongoRssDataList.append(mongoRssData)
+    rssFeedHandler.insertRssFeeds(mongoRssDataList)
 
 
 parser("https://feeds.feedburner.com/ndtvnews-top-stories")
