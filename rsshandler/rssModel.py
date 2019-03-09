d2c22744cc7b5a7d9486490489a07418a3e0b967
Unknown <mmx362003@gmail.com>
2019-03-10 00:31:05 +0800

    112

---
+++ rsshandler/rssModel.py
@@ -0,0 +1,5 @@
+rssFeedList = {}
+rssFeedList['subFeedPaths'] = ['ndtvnews-top-stories', 'ndtvnews-latest', 'ndtvnews-trending-news',
+                               'ndtvnews-india-news', 'ndtvnews-world-news', 'ndtvprofit-latest', 'ndtvmovies-latest', 'ndtvsports-latest']
+rssFeedList['Name'] = 'NdtvNews'
+rssFeedList['url'] = 'https://feeds.feedburner.com/'
