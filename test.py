57f5761548a282c0f543e88bce372a52a05dea7e
Unknown <mmx362003@gmail.com>
2019-03-09 15:15:23 +0800

    change file

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
-array = ['asdsd', 'asdsdsd']
+"""
+import os
+import settings
+import rsshandler.rsshandler
+MONGO_URL = os.environ.get('MONGODB_URI')
+rsshandler.rsshandler.parser(
+    "https://feeds.feedburner.com/ndtvnews-top-stories", MONGO_URL)
+"""
 
 
 def test():
     assert 200 == 200
 
 
-a = "asdssds"
-bb = "asdsdsdsd"
 test()
